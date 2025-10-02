efa78d545e25d23b6272f179e05edbc5b96c41ba
Peterlits Zo <zhouhongyu@ft.tech>
2025-10-02 22:19:55 +0800

    feat: Adjust padding in Navbar styles for better spacing, etc.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,10 @@
-import { useContext, type FC } from "react";
-import { Link } from "react-router";
 import classnames from "classnames";
-
+import { type FC, useContext } from "react";
+import { Link } from "react-router";
+import { Tag } from "../Tag";
 import { ThemeContext } from "../ThemeProvider";
-
 import { components } from "./components.codegen";
 import styles from "./Navbar.module.scss";
-import { Tag } from "../Tag";
 
 interface NavbarProps {
   active: string;
@@ -16,27 +14,37 @@ export const Navbar: FC<NavbarProps> = ({ active }) => {
   const theme = useContext(ThemeContext);
 
   const computedStyles = {
-    '--navbar-link-bg-hover': theme.colors.gray['100'],
-    '--navbar-link-bg-active': theme.colors.blue['000'],
-    '--navbar-link-color-active': theme.colors.blue['900'],
-    '--navbar-border-color': theme.colors.gray['300'],
-    '--navbar-link-color': theme.colors.gray['700'],
+    "--navbar-link-bg-hover": theme.colors.gray["100"],
+    "--navbar-link-bg-active": theme.colors.blue["000"],
+    "--navbar-link-color-active": theme.colors.blue["900"],
+    "--navbar-border-color": theme.colors.gray["300"],
+    "--navbar-link-color": theme.colors.gray["700"],
   };
 
   return (
-    <nav className={styles.navbar} style={computedStyles as React.CSSProperties}>
-      {components.map(component => (
+    <nav
+      className={styles.navbar}
+      style={computedStyles as React.CSSProperties}
+    >
+      {components.map((component) => (
         <Link
           key={component.name}
-          className={classnames(styles.link, component.name === active && styles.active)}
+          className={classnames(
+            styles.link,
+            component.name === active && styles.active,
+          )}
           to={component.path}
         >
           <span>{component.name}</span>
-          {component.wip && <Tag color="red" height="1.25rem">WIP</Tag>}
+          {component.wip && (
+            <Tag color="red" height="1.25rem">
+              WIP
+            </Tag>
+          )}
         </Link>
       ))}
     </nav>
-  )
-}
+  );
+};
 
 Navbar.displayName = "Navbar";
