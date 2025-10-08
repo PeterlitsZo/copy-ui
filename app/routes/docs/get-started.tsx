77cb5aa10f96c39a42e6f90b1b1ab88e8d3cd484
Peterlits Zo <zhouhongyu@ft.tech>
2025-10-08 20:54:27 +0800

    feat: Replace app.css with reset.css and update document for it.

--- app/routes/docs/get-started.tsx
+++ app/routes/docs/get-started.tsx
@@ -1,5 +1,8 @@
+import { CodeHighlight } from "@/components/CodeHighlight";
 import { ComponentTemplate } from "@/components/ComponentTemplate";
+import { ScrollArea } from "@/components/ScrollArea";
 import { Section } from "@/components/Section";
+import { useTheme } from "@/components/ThemeProvider";
 import { Typography } from "@/components/Typography";
 
 export function meta() {
@@ -22,6 +25,7 @@ export default function GetStartedPage() {
           Copy UI's Avatar component document, copy its code and paste it into
           your own project. That's it.
         </Typography.P>
+        <Typography.H2 mt="1rem">Why Copy UI?</Typography.H2>
         <Typography.P>
           Code in Copy UI is stupid enough, so I think it is easy to read and
           change by yourself. I try to build it without any headless UI library,
@@ -54,7 +58,297 @@ export default function GetStartedPage() {
           just because it looks really good. Those projects are under MIT
           License - so this project does the same thing.
         </Typography.P>
+        <Typography.H2 mt="1rem">Reset CSS</Typography.H2>
+        <Typography.P>
+          Copy UI use Tailwind CSS's preflight files. To use Copy UI, you need
+          to copy this CSS file content into your project, name it like
+          reset.css, and import it in your root file:
+        </Typography.P>
+        <ResetCss />
+        <Typography.P>
+          It is copied from Tailwind CSS, but I just remove some comments,
+          replace some magic functions (its `--theme` function) to make it work.
+          You can read{" "}
+          <Link href="https://tailwindcss.com/docs/preflight">
+            its documentation
+          </Link>{" "}
+          to know more about it.
+        </Typography.P>
       </Section.Root>
     </ComponentTemplate>
   );
 }
+
+const Link = ({
+  href,
+  children,
+}: {
+  href: string;
+  children: React.ReactNode;
+}) => {
+  const theme = useTheme();
+  return (
+    <a href={href} style={{ color: theme.colors.blue["600"] }}>
+      {children}
+    </a>
+  );
+};
+
+Link.displayName = "Link";
+
+const ResetCss = () => {
+  const theme = useTheme();
+
+  return (
+    <ScrollArea>
+      <ScrollArea.Viewport
+        style={{
+          height: "40rem",
+          borderRadius: "0.375rem",
+          backgroundColor: theme.colors.gray["000"],
+        }}
+      >
+        <ScrollArea.Content style={{ padding: "1rem", fontSize: "0.875rem" }}>
+          <CodeHighlight code={resetCssCode} lang="css" withLineNumbers />
+        </ScrollArea.Content>
+      </ScrollArea.Viewport>
+      <ScrollArea.Scrollbar>
+        <ScrollArea.Thumb />
+      </ScrollArea.Scrollbar>
+    </ScrollArea>
+  );
+};
+
+ResetCss.displayName = "ResetCss";
+
+const resetCssCode = `\
+@layer base {
+  :root {
+    --font-sans:
+      ui-sans-serif, system-ui, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol',
+      'Noto Color Emoji';
+    --font-serif: ui-serif, Georgia, Cambria, 'Times New Roman', Times, serif;
+    --font-mono:
+      ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New',
+      monospace;
+  }
+
+  *,
+  ::after,
+  ::before,
+  ::backdrop,
+  ::file-selector-button {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    border: 0 solid;
+  }
+
+  html,
+  :host {
+    line-height: 1.5;
+    -webkit-text-size-adjust: 100%;
+    tab-size: 4;
+    font-family: var(--font-sans);
+    -webkit-tap-highlight-color: transparent;
+  }
+
+  hr {
+    height: 0;
+    color: inherit;
+    border-top-width: 1px;
+  }
+
+  abbr:where([title]) {
+    -webkit-text-decoration: underline dotted;
+    text-decoration: underline dotted;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-size: inherit;
+    font-weight: inherit;
+  }
+
+  a {
+    color: inherit;
+    -webkit-text-decoration: inherit;
+    text-decoration: inherit;
+  }
+
+  b,
+  strong {
+    font-weight: bolder;
+  }
+
+  code,
+  kbd,
+  samp,
+  pre {
+    font-family: var(--font-mono);
+    font-size: 1em;
+  }
+
+  small {
+    font-size: 80%;
+  }
+
+  sub,
+  sup {
+    font-size: 75%;
+    line-height: 0;
+    position: relative;
+    vertical-align: baseline;
+  }
+
+  sub {
+    bottom: -0.25em;
+  }
+
+  sup {
+    top: -0.5em;
+  }
+
+  table {
+    text-indent: 0;
+    border-color: inherit;
+    border-collapse: collapse;
+  }
+
+  :-moz-focusring {
+    outline: auto;
+  }
+
+  progress {
+    vertical-align: baseline;
+  }
+
+  summary {
+    display: list-item;
+  }
+
+  ol,
+  ul,
+  menu {
+    list-style: none;
+  }
+
+  img,
+  svg,
+  video,
+  canvas,
+  audio,
+  iframe,
+  embed,
+  object {
+    display: block;
+    vertical-align: middle;
+  }
+
+  img,
+  video {
+    max-width: 100%;
+    height: auto;
+  }
+
+  button,
+  input,
+  select,
+  optgroup,
+  textarea,
+  ::file-selector-button {
+    font: inherit;
+    font-feature-settings: inherit;
+    font-variation-settings: inherit;
+    letter-spacing: inherit;
+    color: inherit;
+    border-radius: 0;
+    background-color: transparent;
+    opacity: 1;
+  }
+
+  :where(select:is([multiple], [size])) optgroup {
+    font-weight: bolder;
+  }
+
+  :where(select:is([multiple], [size])) optgroup option {
+    padding-inline-start: 20px;
+  }
+
+  ::file-selector-button {
+    margin-inline-end: 4px;
+  }
+
+  ::placeholder {
+    opacity: 1;
+  }
+
+  @supports (not (-webkit-appearance: -apple-pay-button)) /* Not Safari */ or
+    (contain-intrinsic-size: 1px) /* Safari 17+ */ {
+    ::placeholder {
+      color: color-mix(in oklab, currentcolor 50%, transparent);
+    }
+  }
+
+  textarea {
+    resize: vertical;
+  }
+
+  ::-webkit-search-decoration {
+    -webkit-appearance: none;
+  }
+
+  ::-webkit-date-and-time-value {
+    min-height: 1lh;
+    text-align: inherit;
+  }
+
+  ::-webkit-datetime-edit {
+    display: inline-flex;
+  }
+
+  ::-webkit-datetime-edit-fields-wrapper {
+    padding: 0;
+  }
+
+  ::-webkit-datetime-edit,
+  ::-webkit-datetime-edit-year-field,
+  ::-webkit-datetime-edit-month-field,
+  ::-webkit-datetime-edit-day-field,
+  ::-webkit-datetime-edit-hour-field,
+  ::-webkit-datetime-edit-minute-field,
+  ::-webkit-datetime-edit-second-field,
+  ::-webkit-datetime-edit-millisecond-field,
+  ::-webkit-datetime-edit-meridiem-field {
+    padding-block: 0;
+  }
+
+  ::-webkit-calendar-picker-indicator {
+    line-height: 1;
+  }
+
+  :-moz-ui-invalid {
+    box-shadow: none;
+  }
+
+  button,
+  input:where([type="button"], [type="reset"], [type="submit"]),
+  ::file-selector-button {
+    appearance: button;
+  }
+
+  ::-webkit-inner-spin-button,
+  ::-webkit-outer-spin-button {
+    height: auto;
+  }
+
+  [hidden]:where(:not([hidden="until-found"])) {
+    /* biome-ignore lint/complexity/noImportantStyles: Just ignore it. */
+    display: none !important;
+  }
+}
+`;
