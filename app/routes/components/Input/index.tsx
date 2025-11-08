0e1c39f296f044df6b516b766e34911f93e62fd6
Peterlits Zo <zhouhongyu@ft.tech>
2025-11-08 19:52:01 +0800

    chore: Move the document of `Input` into a MDX file.

--- app/routes/components/Input/index.tsx
+++ app/routes/components/Input/index.tsx
@@ -1,19 +1,9 @@
 import { useState } from "react";
 
-import { Typography } from "@/components/Typography";
 import { DocLayout } from "@/layouts/DocLayout";
 import type { Route } from "./+types/index";
 import { changelog } from "./changelog.codegen";
-import Demo01 from "./demos/Demo01";
-import demo01SourceCode from "./demos/Demo01.source_code.codegen";
-import Demo02 from "./demos/Demo02";
-import demo02SourceCode from "./demos/Demo02.source_code.codegen";
-import Demo03 from "./demos/Demo03";
-import demo03SourceCode from "./demos/Demo03.source_code.codegen";
-import Demo04 from "./demos/Demo04";
-import demo04SourceCode from "./demos/Demo04.source_code.codegen";
-import Demo05 from "./demos/Demo05";
-import demo05SourceCode from "./demos/Demo05.source_code.codegen";
+import Doc from "./doc.mdx";
 import { sourceCode } from "./source_code.codegen";
 
 export function meta(_: Route.MetaArgs) {
@@ -43,16 +33,7 @@ export default function InputPage() {
       />
       {tabsValue === "doc" && (
         <DocLayout.Content>
-          <DocLayout.Live node={<Demo01 />} code={demo01SourceCode} />
-          <Typography.H2>Examples</Typography.H2>
-          <Typography.H3>Different variants</Typography.H3>
-          <DocLayout.Live node={<Demo02 />} code={demo02SourceCode} />
-          <Typography.H3>Left & right sections</Typography.H3>
-          <DocLayout.Live node={<Demo03 />} code={demo03SourceCode} />
-          <Typography.H3>Disabled state</Typography.H3>
-          <DocLayout.Live node={<Demo04 />} code={demo04SourceCode} />
-          <Typography.H3>Different sizes</Typography.H3>
-          <DocLayout.Live node={<Demo05 />} code={demo05SourceCode} />
+          <Doc />
         </DocLayout.Content>
       )}
       {tabsValue === "source" && (
