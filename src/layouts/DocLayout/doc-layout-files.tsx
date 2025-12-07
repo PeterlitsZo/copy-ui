ad733035034fe6c9c7413eadc7fd7ea9617a495a
Peterlits Zo <zhouhongyu@ft.tech>
2025-12-07 16:43:48 +0800

    chore: Adjust height in `DocLayout.Files` component.

--- src/layouts/DocLayout/doc-layout-files.tsx
+++ src/layouts/DocLayout/doc-layout-files.tsx
@@ -79,7 +79,7 @@ const DocLayoutFiles: FC<DocLayoutFilesProps> = (props) => {
     <>
       <div className={classNames(styles.sourceCode, stx)}>
         {sourceCodeTree}
-        {sourceCodeBlock(true, "90vh")}
+        {sourceCodeBlock(true, "calc(90vh - 3rem)")}
       </div>
       <Modal isOpen={isModalOpen} className={stx}>
         <Modal.Overlay onClick={() => setModalOpen(false)} />
