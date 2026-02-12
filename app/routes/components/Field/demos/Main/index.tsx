df9bb10156972b4b2e4271e55756da56474f141c
Peterlits Zo <zhouhongyu@ft.tech>
2026-02-12 10:29:51 +0800

    docs: Update the demos in `FIeld`'s document.

--- app/routes/components/Field/demos/Main/index.tsx
+++ app/routes/components/Field/demos/Main/index.tsx
@@ -1,27 +1,18 @@
-import { useId } from "react";
-
 import { Field } from "@/components/Field";
 import { Flex } from "@/components/Flex";
+import { Input } from "@/components/Input";
 
 export default function Demo() {
-  const usernameId = useId();
-  const emailId = useId();
-
   return (
     <Flex dir="column" gap="1rem">
       <Field>
-        <Field.Label htmlFor={usernameId}>Username</Field.Label>
-        <Field.Input
-          width="sm"
-          id={usernameId}
-          type="text"
-          placeholder="Enter your username"
-        />
+        <Field.Label>Username</Field.Label>
+        <Input width="sm" type="text" placeholder="Enter your username" />
       </Field>
 
       <Field>
-        <Field.Label htmlFor={emailId}>Email</Field.Label>
-        <Field.Input id={emailId} type="email" placeholder="Enter your email" />
+        <Field.Label>Email</Field.Label>
+        <Input type="email" placeholder="Enter your email" />
         <Field.Description>We'll never share your email.</Field.Description>
       </Field>
 
--- app/routes/components/Field/demos/WithErrorMessage/index.tsx
+++ app/routes/components/Field/demos/WithErrorMessage/index.tsx
@@ -1,10 +1,11 @@
 import { Field } from "@/components/Field";
+import { Input } from "@/components/Input";
 
 export default function Demo() {
   return (
     <Field>
       <Field.Label>My Favorite Fruit</Field.Label>
-      <Field.Input type="text" placeholder="Enter fruit name" />
+      <Input status="error" type="text" placeholder="Enter fruit name" />
       <Field.Error>Sorry that is not my favorite fruit.</Field.Error>
     </Field>
   );
